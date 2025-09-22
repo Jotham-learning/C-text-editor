7809e4e512cfce9164fc4715feb9c0a85ea171ae
Jotham Paul <jothampaulmadhavan@gmail.com>
2025-09-22 10:26:10 +0530

    1.0 text editor done

--- editor.c
+++ editor.c
@@ -2,24 +2,140 @@
 #include <stdlib.h>
 #include <ncurses.h>
 #include <string.h>
-#include <ncurses.h>
-
 #include "gapbuffer.h"
+
 
 struct editorconfig {
     size_t cur_x;
     size_t cur_y;
+    size_t scroll_offset;
+    char *FileName;
+
+    bool redraw_linenos; 
+    WINDOW *TitleBar;
+    WINDOW *TextScreen;
+    WINDOW *lineno;
+    // WINDOW *option;
 } E;
 
 
 
-void configEditor(Text *text){
+void configEditor(Text *text,char *filename){
+
     E.cur_x = E.cur_y = 0;
-    text-> capacity = 2;
+    E.scroll_offset = 0;
+    E.FileName = filename;
+    E.redraw_linenos = TRUE;
+    text-> capacity = 5;
     text-> size = 0;
     text-> lines = calloc(text->capacity,sizeof(Gapbuffer*));
-    insertline(text,E.cur_y);
+    
+    
+    if(text->lines == NULL){ // Added a check for calloc failure
+        free(text);
+        perror("Failed to allocate memory for lines");
+        exit(1);
+    }
+
+    //Title bar because i only need to draw it once
+
+    int lineNo_width = 3;
+    E.TitleBar = newwin(1, COLS, 0, 0);
+    E.lineno = newwin(LINES-1, lineNo_width, 1, 0);
+    E.TextScreen = newwin(LINES-1, COLS-lineNo_width, 1, lineNo_width);
+    int col = getmaxx(stdscr);
+    // title bar
+    int center = (col-strlen(E.FileName)) / 2;
+
+    wattron(E.TitleBar,A_BOLD);
+	 if(!has_colors()) { // without white
+
+        mvwaddnstr(E.TitleBar,0, center,E.FileName,strlen(E.FileName));
+        wrefresh(E.TitleBar);
+
+    }else{//with white color
+
+        start_color();
+        init_pair(1,COLOR_BLACK,COLOR_WHITE);
+        wbkgd(E.TitleBar,COLOR_PAIR(1));
+        mvwaddnstr(E.TitleBar,0, center,E.FileName,strlen(E.FileName));
+        wrefresh(E.TitleBar);
+        
+    }
+    wattroff(E.TitleBar,A_BOLD);
+
+    DrawScreen(NULL);
+
+    insertLine(text,0);
+    FILE *fp = fopen(filename,"r");
+    if (fp != NULL) {
+        int ch;
+        int line =0;
+        int index = 0;
+        while((ch = fgetc(fp)) != EOF){
+            if(ch == '\n'){
+                line +=1;
+                E.cur_y+=1;
+                E.cur_x=0;
+                insertLine(text,line);
+                index = 0;
+                continue;
+            }else{
+                insert(text->lines[line],(char)ch,index);
+                E.cur_x+=1;
+                index+=1;
+            }
+        }
+        fclose(fp);
+    }
 }
+void DrawTilde() {
+    werase(E.lineno);
+    for (int row = 0; row < LINES; row++) {
+        mvwaddch(E.lineno,row,0,'~');
+    }
+
+}
+void DrawScreen(Text *text){
+    if (E.redraw_linenos) {
+        DrawTilde();
+        E.redraw_linenos = false;
+    }
+    if(text == NULL) return; 
+    else{
+        // Draw text
+        int row = getmaxy(E.TextScreen);
+        werase(E.TextScreen);
+        wmove(E.TextScreen,0,0);
+        size_t i;
+        Gapbuffer *buff;
+        for(size_t j = E.scroll_offset; j < text->size && j < row + E.scroll_offset; j++){
+            buff = text->lines[j];
+    
+            for (i = 0; i < buff->gap_start; i++){
+                waddch(E.TextScreen,buff->buffer[i]);
+            }
+            for (i = buff->gap_end; i < buff->buffSize; i++){
+                waddch(E.TextScreen,buff->buffer[i]);  
+            }
+            if(j < text -> size - 1) waddch(E.TextScreen,'\n');
+        }
+        if(E.scroll_offset >0 ){
+            wmove(E.TextScreen,E.cur_y,E.cur_x);
+        }else{
+            wmove(E.TextScreen,E.cur_y-E.scroll_offset,E.cur_x);
+        }
+        
+        wnoutrefresh(E.lineno);
+        wnoutrefresh(E.TextScreen);
+        doupdate();
+    }
+
+
+}
+
+
+// text maniulation
 
 Gapbuffer* createLine(){
     Gapbuffer *gbuf = (Gapbuffer*) malloc(sizeof(Gapbuffer));
@@ -33,11 +149,12 @@ Gapbuffer* createLine(){
     gbuf->gap_end = gbuf->buffSize;
     gbuf->gap_size = gbuf->gap_end - gbuf->gap_start;
     gbuf->size = gbuf->buffSize - gbuf->gap_size;
-    memset(gbuf->buffer,'_',gbuf->buffSize);
+
 
     return gbuf;
 }
-int insertline(Text *text,size_t index){
+
+int insertLine(Text *text,size_t index){
     if(index > text->size) return 1;
     if(text->size == text->capacity){
         resizeText(text);
@@ -47,7 +164,18 @@ int insertline(Text *text,size_t index){
     }
     text->lines[index] = createLine();
     text->size +=1;
-    return 1;
+    return 0;
+}
+int deleteLine(Text *text,size_t index){
+    if(index> text->size) return 1;
+    free(text->lines[index]->buffer);
+    free(text->lines[index]);
+    for(size_t i = index;i < text->size - 1;i++){
+        text->lines[i] = text->lines[i+1];
+    }
+
+    text->size -=1;
+    return 0;
 }
 int resizeText(Text *text){
     text->capacity *= 2;
@@ -58,6 +186,7 @@ int resizeText(Text *text){
     text -> lines = temp;
     return 0;
 }
+
 void destroyText(Text *text){
     for (size_t i = 0; i < text->size; i++){
         if(text->lines[i] != NULL){
@@ -66,8 +195,9 @@ void destroyText(Text *text){
         }
     }
     free(text->lines);
-    
+    free(text);
 }
+
 // gapbuffer manipluation
 int move_left(Gapbuffer *buff,size_t delta){
     if( buff->gap_start >= delta ){
@@ -113,8 +243,7 @@ void resizeBuffer(Gapbuffer *buff, size_t extraSpace){
     temp = realloc(buff->buffer,buff->buffSize + extraSpace);
     if(temp == NULL){
         endwin();
-        fprintf(stderr, "Realloc failed\n");
-        free(buff->buffer); // Free old memory if realloc failed
+        fprintf(stderr, "Resizing buffer : Realloc failed\n");
         exit(1);
     }
     buff->buffer = temp;
@@ -133,7 +262,7 @@ void resizeBuffer(Gapbuffer *buff, size_t extraSpace){
 
 int insert(Gapbuffer *buff,char ch, size_t index){
     
-    if (index < buff->buffSize){
+    if (index <= buff->size){
         if(buff->gap_size <= 1){
             resizeBuffer(buff,10);
         }
@@ -156,100 +285,180 @@ int insert(Gapbuffer *buff,char ch, size_t index){
 }
 
 int delete(Gapbuffer *buff,size_t index){
-    if ( index <= buff->size){
-        int delta = index - buff->gap_start;
-        // to move gapbuffer
-        if (delta > 0){
-            move_right(buff, delta);
-        }
-        else{
-            move_left(buff, abs(delta));  
-        }
-        buff->gap_start -=1;
-        buff->gap_size +=1;
-        buff->size -=1;
-        return 0;
-    }else{
-        return 1;
+    if (index == 0 || index > buff->size) return 1;
+    int delta = index - buff->gap_start;
+    
+    if (delta > 0){
+        move_right(buff, delta);
     }
-}
-
-
-// editor stuff
-void printText(Text *text){
-
-    clear();
-    move(0,0);
-    size_t i;
-    Gapbuffer *buff;
-    for(size_t j = 0;j < text->size;j++){
-        buff = text->lines[j];
-
-        for (i = 0; i < buff->gap_start; i++){
-            addch(buff->buffer[i]);
-        }
-        for (i = buff->gap_end; i < buff->buffSize; i++){
-            addch(buff->buffer[i]);  
-        }
-        addch('\n');
+    else{
+        move_left(buff, abs(delta));  
     }
-    refresh();
-
+    buff->gap_start -=1;
+    buff->gap_size +=1;
+    buff->size -=1;
+    return 0;
 }
 
 void processKeyPress(int ch,Text *text) {
-    getyx(stdscr,E.cur_y,E.cur_x);
-    Gapbuffer *prevline = text->lines[E.cur_y-1];
-    Gapbuffer *currentLine = text->lines[E.cur_y];// get the current line
-    Gapbuffer  *nextline = text->lines[E.cur_y+1];;
+    size_t row = (size_t)getmaxy(E.TextScreen) - 1;
+    getyx(E.TextScreen,E.cur_y,E.cur_x);
+    //size_t row=getmaxy(E.TextScreen);
+    Gapbuffer *prevline;
+    Gapbuffer *currentLine = text->lines[E.cur_y+E.scroll_offset];// get the current line
+    Gapbuffer  *nextline;
     
     if (32 <= ch && ch <= 126) {
         insert(currentLine,(char)ch,E.cur_x);
-        move(E.cur_y, ++E.cur_x); 
+        E.cur_x++;
     }else{
         switch(ch)
         {
             case KEY_RIGHT:
                 if(E.cur_x == currentLine->size) break;
-                move(E.cur_y,++(E.cur_x));
+                E.cur_x++;
                 break;
             case KEY_LEFT:
                 if(E.cur_x == 0) break;
-                move(E.cur_y,--E.cur_x);
+                E.cur_x--;
                 break;
             case KEY_UP:
-                if(E.cur_y == 0) break;
+                if(E.cur_y == 0 && E.scroll_offset ==0) break;
+                prevline = text->lines[(E.cur_y+E.scroll_offset-1)];
+                if(E.cur_y == 0){
+                    E.scroll_offset--;
+                } 
                 if(E.cur_x > prevline->size){
                     E.cur_x = prevline->size;
-                    move(--E.cur_y,E.cur_x);
+                    E.cur_y--;
                     break;
                 }
-                move(--E.cur_y,E.cur_x);
+                if(E.cur_y != 0)E.cur_y--;
                 break;
             case KEY_DOWN:
-                if(E.cur_y + 1 >= text -> size) break;
+                if(E.cur_y + E.scroll_offset + 1 >= text -> size) break;
+                nextline = text->lines[E.cur_y+E.scroll_offset+1];
+                if(E.cur_y == row &&  E.cur_y + E.scroll_offset < text->size){
+                    E.scroll_offset++; 
+                }
                 if(E.cur_x > nextline->size){
                     E.cur_x = nextline->size;
-                    move(++E.cur_y,E.cur_x);
+                    E.cur_y++;
+                    break;
+                }
+                if(E.cur_x != row) E.cur_y++;
+                break;
+            case KEY_BACKSPACE:{
+                if(E.cur_x == 0 && E.cur_y + E.scroll_offset != 0){
+                    prevline = text->lines[(E.cur_y+E.scroll_offset)-1];
+
+                    int old_size = prevline->size;
+
+                    if(currentLine->size != 0){
+
+
+                        move_right(prevline,(prevline->size - prevline->gap_start));
+                        
+
+                        size_t beforeGap = currentLine->gap_start;
+                        size_t afterGap  = currentLine->buffSize - currentLine->gap_end;
+                        size_t totalToMove = beforeGap + afterGap;
+
+
+                        if (prevline->gap_size < totalToMove) {
+                            resizeBuffer(prevline, totalToMove - prevline->gap_size);
+                        }
+
+
+                        memmove(&prevline->buffer[prevline->gap_start],
+                                currentLine->buffer,
+                                beforeGap);
+
+                        memmove(&prevline->buffer[prevline->gap_start + beforeGap],
+                                &currentLine->buffer[currentLine->gap_end],
+                                afterGap);
+
+                        prevline->gap_start += totalToMove;
+                        prevline->gap_size  -= totalToMove;
+                        prevline->size      += totalToMove;
+                    }
+                    deleteLine(text,E.cur_y+E.scroll_offset);
+                    if(E.cur_y == 0){
+                        E.scroll_offset--;
+                    }else{
+                        E.cur_y--;
+                    }
+                    E.cur_x = old_size;
+
+
+                }
+                else if(E.cur_x != 0){
+                    delete(currentLine,E.cur_x--);
                     break;
                 }
                 break;
-            case KEY_BACKSPACE:
-                if(E.cur_x == 0) break;
-                delete(currentLine,E.cur_x);
-                move(E.cur_y,--E.cur_x);
+            }
+            case '\t':
+                for (int i = 0; i < 4; i++) {
+                    insert(currentLine, ' ', E.cur_x);
+                    E.cur_x++;
+                }
                 break;
-
             case 10:  // ASCII newline '\n'
             case 13:  // Carriage return '\r'
-            case KEY_ENTER:
-                insertline(text,++E.cur_y);
-                E.cur_x = 0;
-                move(E.cur_y,E.cur_x);
+            case KEY_ENTER:{
+                if(E.cur_x <= currentLine->size ){
+                    int delta = E.cur_x - currentLine->gap_start;
+                    if (delta > 0) {
+                        move_right(currentLine, delta);
+                    } else {
+                        move_left(currentLine, abs(delta));
+                    }
+    
+                    insertLine(text, E.cur_y + E.scroll_offset + 1);
+                    nextline = text->lines[E.cur_y + E.scroll_offset + 1];
+    
+                    size_t amountToMove = currentLine->buffSize - currentLine->gap_end;
+    
+                    if (nextline->gap_size < amountToMove) {
+                        resizeBuffer(nextline, amountToMove - nextline->gap_size);
+                    }
+    
+    
+                    memmove(&nextline->buffer[nextline->gap_start],
+                            &currentLine->buffer[currentLine->gap_end],
+                            amountToMove);
+    
+    
+                    nextline->gap_start += amountToMove;
+                    nextline->gap_size  -= amountToMove;
+                    nextline->size      += amountToMove;
+    
+    
+                    currentLine->gap_end = currentLine->buffSize;
+                    currentLine->gap_size = currentLine->buffSize - currentLine->gap_start;
+                    currentLine->size = currentLine->gap_start;
+    
+                    E.cur_x = 0;
+                    if(E.cur_y != row) E.cur_y++;
+                    else {
+                        E.scroll_offset++;
+                    }
+                    break;
+                }else{
+                    insertLine(text, E.cur_y + E.scroll_offset + 1);
+                    E.cur_x = 0;
+                    if(E.cur_y != row) E.cur_y++;
+                    else {
+                        E.scroll_offset++;
+                    }                
+                }
                 break;
+            }
+
+
             default:
                 break;
-
         }
     }
 }
@@ -265,7 +474,7 @@ int putTofile(Text *text,char *FileName){
         Gapbuffer *buff = text ->lines[i];
         fwrite(buff->buffer,sizeof(char), buff->gap_start ,fp);
         fwrite(&buff->buffer[buff->gap_end], sizeof(char),buff->buffSize-buff->gap_end,fp);
-        fputc('\n',fp);
+        if(i < text->size-1)fputc('\n',fp);
     }
     fclose(fp);
     return 0;
@@ -281,30 +490,28 @@ int main(int argc, char *argv[]) {
         perror("Text malloc failed");
         exit(1);
     }   
-    configEditor(text);
-    int ch = 0;
-
     initscr();
+    configEditor(text,argv[1]);
+    
     cbreak();
     noecho();
-    keypad(stdscr, TRUE); // for arrow keys
-
+    keypad(E.TextScreen, TRUE); // for arrow keys
+    
+    
+    DrawScreen(text);
+    
+    int ch = 0;
     while (1) {
         
-        ch = getch();
-        if(ch == 'q') break;
+        ch = wgetch(E.TextScreen);
+        if(ch == 24) break;// cntrl-X
         processKeyPress(ch, text);
-        move(0, 0);
-        
-        for(size_t i = 0;i<text->size;i++){
-            printText(text);
-        }
-        move(E.cur_y,E.cur_x);
-        refresh();
-        
+        DrawScreen(text);
+
 
     }
     putTofile(text,argv[1]);
     destroyText(text);
+    delwin(E.TextScreen);
     endwin();
-}
+}// i wrote this in my own text editor :)
--- gapbuffer.h
+++ gapbuffer.h
@@ -1,38 +1,60 @@
 #ifndef GAPBUFFER_H
 #define GAPBUFFER_H
 
-typedef struct
-{   
-    char *buffer;
-    size_t gap_start;
-    size_t gap_end;
-    size_t gap_size;
-    size_t buffSize;
-    size_t size;
-    
-}Gapbuffer;
-typedef struct Text
-{
-    Gapbuffer **lines;
-    size_t size;
-    size_t capacity;
-    
-}Text;
+
+#include <stddef.h>   // for size_t
+
+/* -----------------------------
+   Data Structures
+------------------------------ */
+
+// Gap buffer for each line
+typedef struct {
+    char *buffer;       // character buffer
+    size_t gap_start;   // start of gap
+    size_t gap_end;     // end of gap
+    size_t gap_size;    // current gap size
+    size_t buffSize;    // total buffer size
+    size_t size;        // actual text size (characters in use)
+} Gapbuffer;
+
+// Text structure: dynamic array of line pointers
+typedef struct {
+    Gapbuffer **lines;  // array of line pointers
+    size_t size;        // number of lines
+    size_t capacity;    // allocated capacity
+} Text;
 
 
-void configEditor(Text *text);
+
+
+// === Editor config ===
+void configEditor(Text *text, char *filename);
+void DrawTilde(void);
+void DrawScreen(Text *text);
+
+// === Text manipulation ===
 Gapbuffer* createLine(void);
-int insertline(Text *text,size_t index);
-void initEditor(void);
-void printText(Text *text);
+int insertLine(Text *text, size_t index);
+int deleteLine(Text *text, size_t index);
 int resizeText(Text *text);
+void destroyText(Text *text);
+
+// === Gap buffer manipulation ===
 int move_left(Gapbuffer *buff, size_t delta);
 int move_right(Gapbuffer *buff, size_t delta);
 void resizeBuffer(Gapbuffer *buff, size_t extraSpace);
 int insert(Gapbuffer *buff, char ch, size_t index);
 int delete(Gapbuffer *buff, size_t index);
-void processKeyPress(int ch, Text *);
-int putTofile(Text *text,char *FileName);
+
+// === Input handling ===
+void processKeyPress(int ch, Text *text);
+
+// === File I/O ===
+int putTofile(Text *text, char *FileName);
+
+// === Main entry point ===
 int main(int argc, char *argv[]);
+
 
 #endif
